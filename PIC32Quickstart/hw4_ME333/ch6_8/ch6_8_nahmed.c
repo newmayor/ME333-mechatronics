4515a6ccc8d2c1914a4f8e58f9107317e0a94a06
Numair Ahmed <numairahmed1@gmail.com>
2021-02-25 18:30:07 -0600

    problm 8 chapter 6 fleshing out first pass at setting interrupts

--- PIC32Quickstart/hw4_ME333/ch6_8/ch6_8_nahmed.c
+++ PIC32Quickstart/hw4_ME333/ch6_8/ch6_8_nahmed.c
@@ -1,7 +1,13 @@
 #include "NU32.h"
+#define CORE_TICKS 4000000  // 40 M ticks (one second)
+
 
 int main(void) {
-    
+    NU32_Startup(); // turns on interrupts by default
+
+    __builtin_disable_interrupts();     //disable interrupts at CPU
+    __CP0_SET_COMPARE(CORE_TICKS);      //CPO_COMPARE register set to 40 M
+
 
 
 }
